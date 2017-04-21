7231a0f114101c6334bdaaf7d32cb064cd008f1a
lszafirski <lukasz.szafirski@ac-project.net>
2017-04-22 00:25:27 +0200

    Added admins form

---
+++ Clinic/Clinic.Interface/Admin/SystemRoleSelectControl.cs
@@ -0,0 +1,25 @@
+﻿using Clinic.Facades.Auth;
+using System;
+using System.Windows.Forms;
+
+namespace Clinic.Interface.Admin
+{
+    public partial class SystemRoleSelectControl : UserControl
+    {
+        public SystemRoleSelectControl()
+        {
+            InitializeComponent();
+            AddRadioButtons();
+        }
+
+        private void AddRadioButtons()
+        {
+            var roles = (Role[])Enum.GetValues(typeof(Role));
+            var radio = new RadioButton
+            {
+                Text = radio.Text
+            };
+
+        }
+    }
+}
