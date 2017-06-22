ce969643ad943548a57382f53d923af154d3c7e9
Leszek Gzik (leszgzi982) <leszgzi982@student.polsl.pl>
2017-06-22 12:59:21 +0200

    Formatka asystenta

---
+++ Clinic/Clinic.Interface/Lab/AssistantForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Clinic.Interface.Lab
+{
+    partial class AssistantForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewTests = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTests)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTests
+            // 
+            this.dataGridViewTests.AllowUserToAddRows = false;
+            this.dataGridViewTests.AllowUserToDeleteRows = false;
+            this.dataGridViewTests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTests.Location = new System.Drawing.Point(11, 15);
+            this.dataGridViewTests.Name = "dataGridViewTests";
+            this.dataGridViewTests.ReadOnly = true;
+            this.dataGridViewTests.Size = new System.Drawing.Size(515, 237);
+            this.dataGridViewTests.TabIndex = 0;
+            // 
+            // AssistantForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 367);
+            this.Controls.Add(this.dataGridViewTests);
+            this.Name = "AssistantForm";
+            this.Text = "AssistantForm";
+            this.Load += new System.EventHandler(this.AssistantForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTests)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewTests;
+    }
+}
---
+++ Clinic/Clinic.Interface/Lab/AssistantForm.cs
@@ -0,0 +1,36 @@
+﻿using Clinic.Facades.Tests;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic.Interface.Lab
+{
+    public partial class AssistantForm : Form
+    {
+        public AssistantForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AssistantForm_Load(object sender, EventArgs e)
+        {
+            dataGridViewTests.DataSource = TestService.GetAllScheduled();
+            dataGridViewTests.Columns["Id"].Visible = false;
+            dataGridViewTests.Columns["Result"].Visible = false;
+            dataGridViewTests.Columns["ExecutionDate"].Visible = false;
+            dataGridViewTests.Columns["ManagerNotes"].Visible = false;
+            dataGridViewTests.Columns["ResolutionDate"].Visible = false;
+            dataGridViewTests.Columns["Status"].Visible = false;
+            dataGridViewTests.Columns["IdLabAssistant"].Visible = false;
+            dataGridViewTests.Columns["IdLabManager"].Visible = false;
+            dataGridViewTests.Columns["IdVisit"].Visible = false;
+            dataGridViewTests.Columns["Code"].Visible = false;
+        }
+    }
+}
